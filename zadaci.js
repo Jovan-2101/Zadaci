1ff70191014f5fdbce98a899f2bdd9d66e59583b
Јован <81017518+Jovan-2101@users.noreply.github.com>
2023-08-17 00:37:45 +0200

    Update and rename zadaci.json to zadaci.js

---
+++ zadaci.js
@@ -0,0 +1,22 @@
+dodajZadatak(
+  "Novi zadatak2",
+  "Ovo je opis novog zadatka",
+  "JavaScript",
+  `function 
+  noviZadatak() {\n  
+  // Implementacija novog zadatka\n"<}>"`
+);
+
+dodajZadatak(
+  "Novi zadatak2",
+  "Ovo je opis novog zadatka",
+  "JavaScript",
+  `function noviZadatak() {\n  // Implementacija novog zadatka\n"<}>"`
+);
+
+dodajZadatak(
+  "W:Golden Trophy",
+  "H:100 credits",
+  "Rocket League Trading",
+  `Mrmot123`
+);
