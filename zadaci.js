d3d1a642dbb2d7cadc9797220d198528ab7d5e77
Јован <81017518+Jovan-2101@users.noreply.github.com>
2023-08-16 23:14:42 +0200

    Update zadaci.js

--- zadaci.js
+++ zadaci.js
@@ -1,15 +1,19 @@
-   const htmlSadrzaj = [
-   
+const htmlSadrzaj = [
   {
     naziv: "SUCCESS",
     deskripcija: 'Успјело је',
     jezik: "C",
-    kod: 
-`#include <stdio.h>;
+    kod: `#include <stdio.h>;
 int main(){
   printf("123");
   return 0;
 }`,
   },
-  
+  // ... other elements
 ];
+
+// Convert the array to JSON format
+const jsonHtmlSadrzaj = JSON.stringify(htmlSadrzaj);
+
+// Export the JSON string
+export default jsonHtmlSadrzaj;
