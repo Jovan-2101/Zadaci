bc6210f04ee5dc8099072455f95ca9be798ad8e4
Јован <81017518+Jovan-2101@users.noreply.github.com>
2023-08-16 23:01:39 +0200

    Create zadaci.js

---
+++ zadaci.js
@@ -0,0 +1,15 @@
+   const htmlSadrzaj = [
+   
+  {
+    naziv: "SUCCESS",
+    deskripcija: 'Успјело је',
+    jezik: "C",
+    kod: 
+`#include <stdio.h>;
+int main(){
+  printf("123");
+  return 0;
+}`,
+  },
+  
+];
